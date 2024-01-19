e06fb3e2daedda6793de0c75fba2d476991857d6
Léandre <leandre.colard@gmail.com>
2024-01-19 23:49:33 +0100

    almost done

--- new.py
+++ new.py
@@ -27,40 +27,33 @@ class bruteforce:
         else: 
             print('Erreur lors de la lecture du fichier json')
             exit()
-        
-    def custom_params(self):
-        self.is_custom = bool(input("Do you want to enter custom parameters ?"))
-        if self.is_custom:
-            self.custom_params_setter()
             
     def custom_params_setter(self):
-        lower = input("Include lowercase characters (a-z)? (y/n): ").lower() == 'y'
-        upper = input("Include uppercase characters (A-Z)? (y/n): ").lower() == 'y'
-        numbers = input("Include numbers (0-9)? (y/n): ").lower() == 'y'
-        special = input("Include special characters? (y/n): ").lower() == 'y'
-        dataset = create_dataset(lower, upper, numbers, special)
-        length = int(input("Enter the desired length of combinations: "))
-        final_filename = input("Enter the final filename for the CSV (without .csv extension): ") + '.csv'
-        total_combinations = len(dataset) ** length
-        print(f"Total combinations to generate: {total_combinations}")
-        num_processes = int(input(f"How many processes do you want to use? (1-{multiprocessing.cpu_count()}): "))
+        self.dataset_lower = input("Include lowercase characters (a-z)? (y/n): ").lower() == 'y'
+        self.dataset_upper = input("Include uppercase characters (A-Z)? (y/n): ").lower() == 'y'
+        self.dataset_numbers = input("Include numbers (0-9)? (y/n): ").lower() == 'y'
+        self.dataset_special = input("Include special characters? (y/n): ").lower() == 'y'
+        dataset = self.create_custom_dataset(self.dataset_lower, self.dataset_upper, self.dataset_numbers, self.dataset_special)
+        self.dataset_length = int(input("Enter the desired length of combinations: "))
+        self.final_filename = input("Enter the final filename for the CSV (without .csv extension): ") + '.csv'
+        self.total_combinations = len(dataset) ** self.dataset_length
+        print(f"Total combinations to generate: {self.total_combinations}")
+        self.core_number = int(input(f"How many CPU cores do you want to use? (1-{multiprocessing.cpu_count()}): "))
 
-        
-        
-    def custom_dataset(lower, upper, numbers, special):
+    def create_custom_dataset(self):
         dataset = ''
-        if lower:
+        if self.dataset_lower:
             dataset += 'abcdefghijklmnopqrstuvwxyz'
-        if upper:
+        if self.dataset_upper:
             dataset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        if numbers:
+        if self.dataset_numbers:
             dataset += '0123456789'
-        if special:
+        if self.dataset_special:
             dataset += '!@#$%^&*()-_=+[]{}|;:",.<>?/`~'
         return dataset
     
-    def combination_worker(task):
-        (part_number, dataset, length, start_index, end_index, filename, lock) = task
+    def combination_worker(self):
+        (part_number, dataset, length, start_index, end_index, filename, lock) = self.task
         with open(f"{filename}_part_{part_number}.csv", 'w', newline='') as f:
             writer = csv.writer(f)
             for combo in itertools.islice(itertools.product(dataset, repeat=length), start_index, end_index):
@@ -74,22 +67,25 @@ class bruteforce:
                     with open(part_file, 'rb') as f_part:
                         f_out.write(f_part.read())
                 os.remove(part_file)
+                
+    def multiprocessing_starter(self):
+        partition_size = self.total_combinations // self.core_number
+        manager = multiprocessing.Manager()
+        lock = manager.Lock()
+        self.tasks = [(i, self.dataset, self.length, i * partition_size,
+                (i + 1) * partition_size if i < self.core_number - 1 else self.total_combinations, self.final_filename, lock) for i in
+                range(self.core_number)]
+
+        # Create worker processes for combination generation
+        with multiprocessing.Pool(processes=self.core_number) as pool:
+            pool.map(self.combination_worker, self.tasks)    
+    def is_custom_params(self):
+        self.is_custom = bool(input("Do you want to enter custom parameters ? (y/n): ").lower()) == 'y'
+        if self.is_custom:
+            self.custom_params_setter()
+        else: 
+            self.combination_worker()
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-    def test_values(self):
-        print(self.dataset, self.length, self.total_combinations, self.core_number)
-            
-bruteforce().test_values()
+bruteforce().is_custom_params()
 
             
